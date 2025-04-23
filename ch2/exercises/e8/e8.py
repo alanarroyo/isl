2833ae5a08994dccf23d47e17cea4fcd56e6c7d8
alanarroyo <alanarroyoguevara@gmail.com>
2025-04-23 08:24:20 +0000

    ch2e8g completed

--- ch2/exercises/e8/e8.py
+++ ch2/exercises/e8/e8.py
@@ -45,3 +45,15 @@ print(college['Elite'].value_counts())
 fig, ax = subplots(figsize = (8,8))
 college.boxplot('Outstate', by = 'Elite', ax=ax)
 fig.savefig('boxplot_elite.png')
+
+# (g) multiple plot
+
+fig, axes = subplots(nrows = 2, 
+                     ncols =3, 
+                     figsize =(16,8) )
+axes[0,0].hist(college['Outstate'])
+axes[0,1].hist(college['PhD'])
+axes[1,0].hist(college['Apps'])
+axes[1,1].hist(college['Accept'])
+
+fig.savefig('multiple_hist.png')
