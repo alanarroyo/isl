c8fa1e0c4e6751ac33275f3290a1bcf4cb841887
alanarroyo <alanarroyoguevara@gmail.com>
2025-04-25 20:12:23 +0000

    ch2e9 added scatterplot mpg vs horsepower

--- ch2/exercises/e9/e9.py
+++ ch2/exercises/e9/e9.py
@@ -73,3 +73,16 @@ print(col_info)
 fig, ax = subplots() 
 pd.plotting.scatter_matrix(Auto, ax=ax, figsize=(100,100 ), alpha=0.7 )
 fig.savefig('scatter_matrix.png')
+plt.scatter(Auto.horsepower, Auto.mpg, alpha=0.7,
+           facecolors= 'none', edgecolors ='b' )
+plt.savefig('scatterplot_mpg_vs_hoserpower.png', dpi=100)
+"""
+Observations:
+    - displacement horsepower and weight seem to be positively correlated.
+    - Previous 3 predictors seem to also be relared to the number of cylinders. 
+    - These three variables are inversely related to mpg. This kind of make sense
+. 
+- It is lessclear if these tgre variables are inversely related to acceleration.
+- There also seem to be some correlatin between mpg and year (the newer the more efficients are the cars?
+
+"""
