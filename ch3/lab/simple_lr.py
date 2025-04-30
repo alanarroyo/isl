78377f5975ad382c758f4f974853af9a274625f5
alanarroyo <alanarroyoguevara@gmail.com>
2025-04-30 09:01:07 +0200

    ch3lab simple regression started

--- ch3/lab/simple_lr.py
+++ ch3/lab/simple_lr.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 from matplotlib.pyplot import subplots
 import matplotlib
+import matplotlib.pyplot as plt
 import statsmodels.api as sm
 from statsmodels.stats.outliers_influence \
 	import variance_inflation_factor as VIF
@@ -10,6 +11,7 @@ from ISLP import load_data
 from ISLP.models import (ModelSpec as MS,
 			summarize,
 			poly)
+from sklearn.linear_model import LinearRegression
 
 Boston = load_data("Boston")
 print(Boston.columns)
@@ -30,7 +32,7 @@ X = design.transform(Boston)
 
 print(X.head())
 
-print(results.summary())
+#print(results.summary())
 print(results.params)
 
 # predictions
@@ -41,3 +43,30 @@ print(newX)
 new_predictions = results.get_prediction(newX)
 print(new_predictions.predicted_mean)
 print( new_predictions.conf_int(alpha = 0.05))
+
+reg = LinearRegression(fit_intercept=False)
+reg.fit(X, y)
+
+print('intercept:', reg.intercept_)
+print('coefficients: ', reg.coef_)
+print('predictions: ', reg.predict(newX))
+
+# plot line
+
+def abline(ax, b, m, *args, **kwargs):
+    xlim = ax.get_xlim()
+    ylim = [m + xlim[0] + b, m * xlim[1] + b]
+    ax.plot(xlim, ylim, *args, **kwargs)
+   
+fig, ax = subplots(figsize=(8,8))
+ax.scatter(Boston.lstat, Boston.medv,
+            facecolors = 'none',
+            edgecolors = 'k')
+abline(ax, results.params.values[0], results.params.values[1],
+
+    linewidth=3,
+    color = 'r')
+ax.set_xlabel('lstat: % lower status of the population ')
+ax.set_ylabel("medv: Median value of home (in $1000's)")
+ax.set_title('A simple regression')
+fig.savefig('simple_reg.png')
